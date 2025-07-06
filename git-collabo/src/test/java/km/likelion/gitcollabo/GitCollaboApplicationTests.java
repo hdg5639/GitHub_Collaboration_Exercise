71c2f20bec2ff228e3db96a33b8feb99b01bf157
DonggyunHan <hdg5639@daum.net>
2025-07-06 20:52:28 +0900

    feat: 초기 프로젝트 생성 - 초기 스프링 프로젝트생성

---
+++ git-collabo/src/test/java/km/likelion/gitcollabo/GitCollaboApplicationTests.java
@@ -0,0 +1,13 @@
+package km.likelion.gitcollabo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitCollaboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
