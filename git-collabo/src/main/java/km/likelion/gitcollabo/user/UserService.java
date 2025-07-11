b894b0a7799cb318ada095a054599b8bfe6d160f
seojin57913 <seojin57913@naver.com>
2025-07-11 17:49:56 +0900

    feat: User테이블 - UserRepository 생성 - UserEntity (nullable = false) 추가 - UserController, UserService 구현

--- git-collabo/src/main/java/km/likelion/gitcollabo/user/UserService.java
+++ git-collabo/src/main/java/km/likelion/gitcollabo/user/UserService.java
@@ -1,4 +1,40 @@
 package km.likelion.gitcollabo.user;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
 public class UserService {
+    private final UserRepository userRepository;
+
+    public UserDTO addUser(UserDTO user) {
+        UserDTO userDTO = userRepository.save(UserEntity.builder()
+                .id(user.getId())
+                .userName(user.getUserName())
+                .email(user.getEmail())
+                .password(user.getPassword())
+                .build())
+                .toDTO();
+        return userDTO;
+    }
+
+    public UserDTO getUser(Long id) {
+        UserEntity user = userRepository.findById(id)
+                .orElseThrow(()-> new RuntimeException("User not found"));
+        return user.toDTO();
+    }
+
+    public UserDTO updateUser(UserDTO userDTO) {
+        UserEntity user = userRepository.findById(userDTO.getId())
+                .orElseThrow(()-> new RuntimeException("User not found"));
+        user.setUserName(userDTO.getUserName());
+        user.setEmail(userDTO.getEmail());
+        user.setPassword(userDTO.getPassword());
+        return user.toDTO();
+    }
+
+    public void deleteUser(Long id) {
+        userRepository.deleteById(id);
+    }
 }
